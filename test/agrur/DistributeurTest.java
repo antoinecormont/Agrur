e2ab74233ad09a05af3c63ffac613bacbda4190c
sio <sio@poste6314-13.SIO2>
2015-03-20 10:55:17 +0100

    test unitaires classe Distributeur

--- test/agrur/DistributeurTest.java
+++ test/agrur/DistributeurTest.java
@@ -15,6 +15,8 @@ import junit.framework.TestCase;
 public class DistributeurTest extends TestCase {
     
     private Distributeur leDistributeur;
+    private Commande laCommande;
+    ArrayList<Commande> lesCommandes = new ArrayList<Commande>();
     
     public DistributeurTest(String testName) {
         super(testName);
@@ -27,12 +29,61 @@ public class DistributeurTest extends TestCase {
     protected void setUp() throws Exception {
         String unId = "carr15432";
         String unNom="CORMONT";
-        ArrayList<Commande> lesCommandes = new ArrayList<Commande>;
+        ArrayList<Commande> lesCommandes = new ArrayList<Commande>();
         
         leDistributeur = new Distributeur(unId,unNom,lesCommandes);
         
+        int idCmd=1;
+        Produit unProduit = new Produit ("Noix de Grenoble","Noix",5);
+        double unPrixHT=12.4;
+        String unConditionnement="Sachet";
+        int uneQTE=3;
+        String uneDateConditionnement="2014-03-13";
+        String uneDateEnvoi="2014-03-13";
+        
+        laCommande = new Commande(idCmd,unProduit,unPrixHT,unConditionnement,uneQTE,uneDateConditionnement);
         
     }
+    
+    public void testGetId(){
+        String idAttendu = "carr15432";
+        assertEquals("Les id sont différents",idAttendu,leDistributeur.getId());
+    }
+    
+    public void testGetNom(){
+        String nomAttendu="CORMONT";
+        assertEquals("Les noms sont différents",nomAttendu,leDistributeur.getNom());
+    }
+    
+    public void testAddCommande(){
+        leDistributeur.addCommande(laCommande);
+        for(Commande uneCommande : lesCommandes){
+            assertEquals("La commande n'a pas été ajoutée ",uneCommande.getId(),uneCommande.getId());
+        }
+    }
+    
+    public void testGetCommande(){
+        for (Commande uneCommande:lesCommandes){
+            assertEquals("La commande n'a pas été trouvée",uneCommande.getId(),laCommande.getId());
+        }
+    } 
+    
+    public void testGetCommandesEnCours(){
+        int idCmdEnCoursAttendu=1;
+        for(Commande uneCommande:lesCommandes){
+             assertEquals("La commande en cours n'a pas été trouvée",idCmdEnCoursAttendu,uneCommande.getId());
+        }
+    }
+    
+    public void testGetCommandesExpediees(){
+        int idCmdExpedieeAttendu=1;
+        for (Commande uneCommande:lesCommandes){
+            assertEquals("La commande expédiée n'a pas été trouvée",idCmdExpedieeAttendu,uneCommande.getId());
+        }
+                
+    }
+    
+    
     
     
     
