89bd902dda804a85e7ad48315f97ca9ee3de9aa6
sio <sio@poste6314-13.SIO2>
2015-03-20 10:54:40 +0100

    test unitaires classe Produit

---
+++ test/agrur/ProduitTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agrur;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author sio
+ */
+public class ProduitTest extends TestCase {
+    
+    public ProduitTest(String testName) {
+        super(testName);
+    }
+
+    public void testSomeMethod() {
+    }
+    
+}
